e919d37ff4f8aaddf3d542ed103b5373f61b1ae0
SteffenMTP <stef303n@videndjurs.net>
2023-03-23 09:32:58 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // './**/*.html',
+    './index.html', 
+    './pages/test.html'],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#17171F',
+        'selected-text': '#A3A3FF',
+        'theme': '#3F3FFF',
+        'nav': '#404053',
+        'secondary': '#9191A4',
+        'badge': '#3F3F51',
+        'input-border': '#565666',
+        'input': '#2A2A35'
+      }
+    },
+    fontFamily: {
+      'barlow': ["'barlow'", 'sans-serif']
+    }
+  },
+}
